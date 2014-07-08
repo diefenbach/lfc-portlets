4f5dd7820830c54da0206015252cb84258eadff5
Kai Diefenbach <kai.diefenbach@iqpp.de>
2014-07-08 11:56:12 +0200

    Updates version of django-portles to 1.2.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0.1'
+version = '1.0.2'
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.txt')).read()
@@ -29,6 +29,6 @@ setup(
     zip_safe=False,
     install_requires=[
         'setuptools',
-        'django-portlets == 1.1.1',
+        'django-portlets == 1.2',
     ],
 )
